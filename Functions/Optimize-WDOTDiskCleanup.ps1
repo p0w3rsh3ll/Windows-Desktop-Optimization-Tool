3eb94b5b557c306b7e2b09f6c8e87bd60cfdeab5
Tim Muessig <timuessi@microsoft.com>
2025-08-13 08:01:03 -0500

    Staging functions

---
+++ Functions/Optimize-WDOTDiskCleanup.ps1
@@ -0,0 +1,21 @@
+function Optimize-WDOTDiskCleanup
+{
+    [CmdletBinding()]
+
+    Param
+    (
+
+    )
+
+    Begin
+    {
+        Write-Verbose "Entering Function '$($MyInvocation.MyCommand.Name)'"
+    }
+    Process
+    {
+    }
+    End
+    {
+        Write-Verbose "Exiting Function '$($MyInvocation.MyCommand.Name)'"
+    }
+}
---
+++ Functions/Optimize-WDOTLocalPolicySettings.ps1
@@ -0,0 +1,21 @@
+function Optimize-WDOTLocalPolicySettings
+{
+    [CmdletBinding()]
+
+    Param
+    (
+
+    )
+
+    Begin
+    {
+        Write-Verbose "Entering Function '$($MyInvocation.MyCommand.Name)'"
+    }
+    Process
+    {
+    }
+    End
+    {
+        Write-Verbose "Exiting Function '$($MyInvocation.MyCommand.Name)'"
+    }
+}
