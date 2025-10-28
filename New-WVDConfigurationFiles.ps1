2136d06113776f7df01ad168329e615f35e909cd
Tim Muessig <timuessi@microsoft.com>
2025-10-28 13:35:47 -0500

    Adding templates and functions to control them

---
+++ New-WVDConfigurationFiles.ps1
@@ -0,0 +1,39 @@
+﻿Param ($FolderName)
+Function New-WVDConfigurationFiles
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        $FolderName
+
+    )
+    Begin
+    {
+        
+    }
+
+    Process
+    {
+
+        $TargetFolder = "$PSScriptRoot\Configurations\$FolderName"
+        If (-not(Test-Path $TargetFolder))
+        {
+            New-Item $TargetFolder -ItemType Directory | Out-Null
+            Copy-Item "$PSScriptRoot\Configurations\Templates\*.*" "$PSScriptRoot\Configurations\$FolderName"
+        }
+        Else
+        {
+            Write-Warning "Folder $FolderName already exists, exiting script!"
+        }
+        
+        
+    }
+
+    End
+    {
+
+    }
+}
+
+New-WVDConfigurationFiles -FolderName $FolderName
---
+++ Set-WVDConfigurations.ps1
@@ -0,0 +1,66 @@
+﻿Param (
+    $ConfigurationFile,
+    $ConfigFolderName
+)
+Function Set-WVDConfigurations
+{
+    [Cmdletbinding()]
+    Param 
+    (
+        [Parameter()]
+        $ConfigurationFile,
+        
+        [Parameter()]
+        $ConfigFolderName
+    )
+
+    Begin {
+        Switch -WildCard ($ConfigurationFile)
+        {
+            AppxPackages*        {$PropName = "AppxPackage"}
+            AutoLoggers*         {$PropName = "KeyName"}
+            DefaultUserSettings* {$PropName = "KeyName"}
+            EdgsSettings*        {$PropName = "RegItemValueName"}
+            LanManWorkstation*   {$PropName = ""}
+            PolicyRegSettings*   {$PropName = "RegItemValueName" }
+            ScheduledTasks*      {$PropName = "ScheduledTask"}
+            Services*            {$PropName = "Name"}
+        }
+    }
+
+    Process 
+    {
+        If (-not($ConfigurationFile.endsWith(".json") ))
+        {
+            $ConfigurationFile = "{0}.json" -f $ConfigurationFile
+        }
+        $TargetFile = "$PSScriptRoot\Configurations\$ConfigFolderName\$ConfigurationFile"
+        if (Test-Path $TargetFile)
+        {
+            $Content = Get-Content $TargetFile -Raw | ConvertFrom-Json
+            foreach ($Config in $Content)
+            {
+                #$FirstProp = $Config | Get-Member -MemberType NoteProperty | Select-Object -First 1
+                $Name = $Config."$($PropName)"
+                $Response = Read-Host "Enable '$Name'? [A]pply or [Enter] for 'Skip'"
+                if ($Response -eq 'A' -or $Response -eq "a")
+                {
+                    $Config.OptimizationState = 'Apply'
+                }
+                else 
+                {
+                    $Config.OptimizationState = 'Skip'
+                }
+            }
+            $Content | ConvertTo-Json -Depth 10 | Set-Content $TargetFile
+        }
+        else 
+        {
+            Write-Warning "File $TargetFile not found, exiting script!"    
+        }
+    }
+
+    End {}
+}
+
+Set-WVDConfigurations -ConfigurationFile $ConfigurationFile -ConfigFolderName $ConfigFolderName 
