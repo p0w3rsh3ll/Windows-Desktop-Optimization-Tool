a5f85506ce14ab107eda39b3062148f6d1510ab3
Emin <emin.atac@gmail.com>
2025-12-24 14:44:38 +0100

    Fixed 6 violations of PSAvoidTrailingWhitespace rule

--- Functions/Remove-WDOTWindowsMediaPlayer.ps1
+++ Functions/Remove-WDOTWindowsMediaPlayer.ps1
@@ -12,17 +12,17 @@
     {
         try
         {
-            Write-EventLog -EventId 10 -Message "[Windows Optimize] Disable / Remove Windows Media Player" -LogName 'WDOT' -Source 'WindowsMediaPlayer' -EntryType Information 
+            Write-EventLog -EventId 10 -Message "[Windows Optimize] Disable / Remove Windows Media Player" -LogName 'WDOT' -Source 'WindowsMediaPlayer' -EntryType Information
             Write-Host "[Windows Optimize] Disable / Remove Windows Media Player" -ForegroundColor Cyan
             Disable-WindowsOptionalFeature -Online -FeatureName WindowsMediaPlayer -NoRestart | Out-Null
-            Get-WindowsPackage -Online -PackageName "*Windows-mediaplayer*" | ForEach-Object { 
-                Write-EventLog -EventId 10 -Message "Removing $($_.PackageName)" -LogName 'WDOT' -Source 'WindowsMediaPlayer' -EntryType Information 
+            Get-WindowsPackage -Online -PackageName "*Windows-mediaplayer*" | ForEach-Object {
+                Write-EventLog -EventId 10 -Message "Removing $($_.PackageName)" -LogName 'WDOT' -Source 'WindowsMediaPlayer' -EntryType Information
                 Remove-WindowsPackage -PackageName $_.PackageName -Online -ErrorAction SilentlyContinue -NoRestart | Out-Null
             }
         }
-        catch 
-        { 
-            Write-EventLog -EventId 110 -Message "Disabling / Removing Windows Media Player - $($_.Exception.Message)" -LogName 'WDOT' -Source 'WindowsMediaPlayer' -EntryType Error 
+        catch
+        {
+            Write-EventLog -EventId 110 -Message "Disabling / Removing Windows Media Player - $($_.Exception.Message)" -LogName 'WDOT' -Source 'WindowsMediaPlayer' -EntryType Error
         }
     }
 
