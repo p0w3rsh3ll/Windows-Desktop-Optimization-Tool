916d0fd0ddadf297f4bebd15f9e49df5f7888cfb
Emin <emin.atac@gmail.com>
2025-12-24 14:42:27 +0100

    Fixed 14 violations of PSAvoidTrailingWhitespace rule

--- Functions/Remove-WDOTAppxPackages.ps1
+++ Functions/Remove-WDOTAppxPackages.ps1
@@ -16,7 +16,7 @@
         $AppxConfigFilePath = ".\AppxPackages.json"
         If (Test-Path $AppxConfigFilePath)
         {
-            Write-EventLog -EventId 20 -Message "[Windows Optimize] Removing Appx Packages" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Information 
+            Write-EventLog -EventId 20 -Message "[Windows Optimize] Removing Appx Packages" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Information
             Write-Host "[Windows Optimize] Removing Appx Packages" -ForegroundColor Cyan
             $AppxPackage = (Get-Content $AppxConfigFilePath | ConvertFrom-Json).Where( { $_.OptimizationState -eq 'Apply' })
             If ($AppxPackage.Count -gt 0)
@@ -24,36 +24,36 @@
                 Foreach ($Item in $AppxPackage)
                 {
                     try
-                    {                
-                        Write-EventLog -EventId 20 -Message "Removing Provisioned Package $($Item.AppxPackage)" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Information 
+                    {
+                        Write-EventLog -EventId 20 -Message "Removing Provisioned Package $($Item.AppxPackage)" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Information
                         Write-Verbose "Removing Provisioned Package $($Item.AppxPackage)"
                         Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -like ("*{0}*" -f $Item.AppxPackage) } | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Out-Null
-                        
-                        Write-EventLog -EventId 20 -Message "Attempting to remove [All Users] $($Item.AppxPackage) - $($Item.Description)" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Information 
+
+                        Write-EventLog -EventId 20 -Message "Attempting to remove [All Users] $($Item.AppxPackage) - $($Item.Description)" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Information
                         Write-Verbose "Attempting to remove [All Users] $($Item.AppxPackage) - $($Item.Description)"
-                        Get-AppxPackage -AllUsers -Name ("*{0}*" -f $Item.AppxPackage) | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue 
-                        
-                        Write-EventLog -EventId 20 -Message "Attempting to remove $($Item.AppxPackage) - $($Item.Description)" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Information 
+                        Get-AppxPackage -AllUsers -Name ("*{0}*" -f $Item.AppxPackage) | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+
+                        Write-EventLog -EventId 20 -Message "Attempting to remove $($Item.AppxPackage) - $($Item.Description)" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Information
                         Write-Verbose "Attempting to remove $($Item.AppxPackage) - $($Item.Description)"
                         Get-AppxPackage -Name ("*{0}*" -f $Item.AppxPackage) | Remove-AppxPackage -ErrorAction SilentlyContinue | Out-Null
                     }
-                    catch 
+                    catch
                     {
-                        Write-EventLog -EventId 120 -Message "Failed to remove Appx Package $($Item.AppxPackage) - $($_.Exception.Message)" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Error 
+                        Write-EventLog -EventId 120 -Message "Failed to remove Appx Package $($Item.AppxPackage) - $($_.Exception.Message)" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Error
                         Write-Warning "Failed to remove Appx Package $($Item.AppxPackage) - $($_.Exception.Message)"
                     }
                 }
             }
-            Else 
+            Else
             {
-                Write-EventLog -EventId 20 -Message "No AppxPackages found to disable" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Warning 
+                Write-EventLog -EventId 20 -Message "No AppxPackages found to disable" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Warning
                 Write-Warning "No AppxPackages found to disable in $AppxConfigFilePath"
             }
         }
-        Else 
+        Else
         {
 
-            Write-EventLog -EventId 20 -Message "Configuration file not found - $AppxConfigFilePath" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Warning 
+            Write-EventLog -EventId 20 -Message "Configuration file not found - $AppxConfigFilePath" -LogName 'WDOT' -Source 'AppxPackages' -EntryType Warning
             Write-Warning "Configuration file not found -  $AppxConfigFilePath"
         }
 
