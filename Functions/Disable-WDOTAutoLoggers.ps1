e0584ce0c9606bbf6e899aa8604378a8d7a3ecee
Emin <emin.atac@gmail.com>
2025-12-27 20:57:02 +0100

    Disable-WDOTAutoLoggers.ps1 improvements

--- Functions/Disable-WDOTAutoLoggers.ps1
+++ Functions/Disable-WDOTAutoLoggers.ps1
@@ -1,57 +1,52 @@
-﻿function Disable-WDOTAutoLogger
-{
-    [CmdletBinding()]
-
-    Param
-    (
-
-    )
-
-    Begin
-    {
-        Write-Verbose "Entering Function '$($MyInvocation.MyCommand.Name)'"
-    }
-    Process
-    {
-        $AutoLoggersFilePath = ".\Autologgers.Json"
-        If (Test-Path $AutoLoggersFilePath)
-        {
-            Write-EventLog -EventId 50 -Message "Disable AutoLoggers" -LogName 'WDOT' -Source 'AutoLoggers' -EntryType Information
-            Write-Host "[Windows Optimize] Disable Autologgers" -ForegroundColor Cyan
-            $DisableAutologgers = (Get-Content $AutoLoggersFilePath | ConvertFrom-Json).Where( { $_.OptimizationState -eq 'Apply' })
-            If ($DisableAutologgers.count -gt 0)
-            {
-                Write-EventLog -EventId 50 -Message "Disable AutoLoggers" -LogName 'WDOT' -Source 'AutoLoggers' -EntryType Information
-                Write-Verbose "Processing Autologger Configuration File"
-                Foreach ($Item in $DisableAutologgers)
-                {
-                    Write-EventLog -EventId 50 -Message "Updating Registry Key for: $($Item.KeyName)" -LogName 'WDOT' -Source 'AutoLoggers' -EntryType Information
-                    Write-Verbose "Updating Registry Key for: $($Item.KeyName)"
-                    Try
-                    {
-                        New-ItemProperty -Path ("{0}" -f $Item.KeyName) -Name "Start" -PropertyType "DWORD" -Value 0 -Force -ErrorAction Stop | Out-Null
+﻿#Requires -RunAsAdministrator
+Function Disable-WDOTAutoLogger {
+ [CmdletBinding()]
+ Param(
+  [Parameter()]
+  [string]$AutoLoggersFilePath = ".\Autologgers.Json"
+ )
+ Begin {
+  Write-Verbose -Message "Entering Function '$($MyInvocation.MyCommand.Name)'"
+  $HT = @{ ErrorAction = 'Stop' }
+  $sHT = @{ ErrorAction = 'SilentlyContinue' }
+  $EVT = @{ LogName = 'WDOT' ; Source = 'AutoLoggers' }
+ }
+ Process {
+        If (Test-Path -Path $AutoLoggersFilePath -PathType Leaf) {
+            Write-EventLog -EventId 50 -Message 'Disable AutoLoggers' -EntryType Information @EVT @sHT
+            Write-Host -Object "[Windows Optimize] Disable Autologgers" -ForegroundColor Cyan
+            try {
+             $DisableAutologgers = (Get-Content -Path $AutoLoggersFilePath @HT | ConvertFrom-Json @HT).Where({ $_.OptimizationState -eq 'Apply' })
+             Write-Verbose -Message "Successfully read Autologgers from $($AutoLoggersFilePath)"
+            } catch {
+             Write-Warning -Message "Failed to read Autologgers from $($AutoLoggersFilePath) because $($_.Exception.Message)"
+            }
+            If ($DisableAutologgers.count -gt 0) {
+                Write-EventLog -EventId 50 -Message "Disable AutoLoggers" -EntryType Information @EVT  @sHT
+                Write-Verbose -Message 'Processing Autologger Configuration File'
+                $DisableAutologgers |
+                Foreach-Object {
+                    $Item = $_
+                    Write-EventLog -EventId 50 -Message "Updating Registry Key for: $($Item.KeyName)" -EntryType Information @EVT @sHT
+                    Write-Verbose -Message "Updating Registry Key for: $($Item.KeyName)"
+                    try {
+                        $null = New-ItemProperty -Path ("{0}" -f $Item.KeyName) -Name 'Start' -PropertyType 'DWORD' -Value 0 -Force @HT
+                        Write-Verbose -Message "Successfully disabled autologger: $(Split-Path -Path $Item.KeyName -Leaf)"
+                    } catch {
+                        Write-Warning -Message "Failed to add $($Item.KeyName) because $($_.Exception.Message)"
+                        Write-EventLog -EventId 150 -Message "Failed to add $($Item.KeyName)`n`n $($_.Exception.Message)" -EntryType Error @EVT @sHT
                     }
-                    Catch
-                    {
-                        Write-EventLog -EventId 150 -Message "Failed to add $($Item.KeyName)`n`n $($Error[0].Exception.Message)" -LogName 'WDOT' -Source 'AutoLoggers' -EntryType Error
-                    }
-
                 }
+            } else {
+                Write-EventLog -EventId 50 -Message 'No Autologgers found to disable' -EntryType Warning @EVT @sHT
+                Write-Verbose -Message 'No Autologgers found to disable'
             }
-            Else
-            {
-                Write-EventLog -EventId 50 -Message "No Autologgers found to disable" -LogName 'WDOT' -Source 'AutoLoggers' -EntryType Warning
-                Write-Verbose "No Autologgers found to disable"
-            }
+        } else {
+            Write-EventLog -EventId 150 -Message "File not found: $AutoLoggersFilePath" -EntryType Error @EVT @sHT
+            Write-Warning -Message "File Not Found: $AutoLoggersFilePath"
         }
-        Else
-        {
-            Write-EventLog -EventId 150 -Message "File not found: $AutoLoggersFilePath" -LogName 'WDOT' -Source 'AutoLoggers' -EntryType Error
-            Write-Warning "File Not Found: $AutoLoggersFilePath"
-        }
-    }
-    End
-    {
-        Write-Verbose "Exiting Function '$($MyInvocation.MyCommand.Name)'"
-    }
+ }
+ End {
+        Write-Verbose -Message "Exiting Function '$($MyInvocation.MyCommand.Name)'"
+ }
 }
