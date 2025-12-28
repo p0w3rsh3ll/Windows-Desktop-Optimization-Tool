b6c24ab6801d0e1133ee843118ea18060d9d7879
Emin <emin.atac@gmail.com>
2025-12-28 17:05:39 +0100

    Optimize-WDOTDiskCleanup.ps1 lisible improvements

--- Functions/Optimize-WDOTDiskCleanup.ps1
+++ Functions/Optimize-WDOTDiskCleanup.ps1
@@ -1,64 +1,61 @@
-function Optimize-WDOTDiskCleanup
-{
+#Requires -RunAsAdministrator
+Function Optimize-WDOTDiskCleanup {
     [CmdletBinding()]
-
-    Param
-    (
-
-    )
-
+    Param()
     Begin
     {
-        Write-Verbose "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        $sHT = @{ ErrorAction = 'SilentlyContinue' }
+        EVT = @{ LogName = 'WDOT' ; Source = 'DiskCleanup' }
     }
     Process
     {
         try
         {
-            Write-EventLog -EventId 90 -Message "Removing .tmp, .etl, .evtx, thumbcache*.db, *.log files not in use" -LogName 'WDOT' -Source 'DiskCleanup' -EntryType Information
-            Write-Verbose "Removing .tmp, .etl, .evtx, thumbcache*.db, *.log files not in use"
-            Get-ChildItem -Path c:\ -Include *.tmp, *.dmp, *.etl, *.evtx, thumbcache*.db, *.log -File -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -ErrorAction SilentlyContinue
+            Write-EventLog -EventId 90 -Message "Removing .tmp, .etl, .evtx, thumbcache*.db, *.log files not in use" @EVT -EntryType Information @sHT
+            Write-Verbose -Message 'Removing .tmp, .etl, .evtx, thumbcache*.db, *.log files not in use'
+            Get-ChildItem -Path 'C:\' -Include *.tmp, *.dmp, *.etl, *.evtx, thumbcache*.db, *.log -File -Recurse -Force @sHT | Remove-Item @sHT
 
             # Delete "RetailDemo" content (if it exits)
-            Write-EventLog -EventId 90 -Message "Removing Retail Demo content (if it exists)" -LogName 'WDOT' -Source 'DiskCleanup' -EntryType Information
-            Write-Verbose "Removing Retail Demo content (if it exists)"
-            Get-ChildItem -Path $env:ProgramData\Microsoft\Windows\RetailDemo\* -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -Recurse -ErrorAction SilentlyContinue
+            Write-EventLog -EventId 90 -Message "Removing Retail Demo content (if it exists)" @EVT -EntryType Information @sHT
+            Write-Verbose -Message 'Removing Retail Demo content (if it exists)'
+            Get-ChildItem -Path $env:ProgramData\Microsoft\Windows\RetailDemo\* -Recurse -Force @sHT | Remove-Item -Recurse @sHT
 
             # Delete not in-use anything in the C:\Windows\Temp folder
-            Write-EventLog -EventId 90 -Message "Removing all files not in use in $env:windir\TEMP" -LogName 'WDOT' -Source 'DiskCleanup' -EntryType Information
-            Write-Verbose "Removing all files not in use in $env:windir\TEMP"
-            Remove-Item -Path $env:windir\Temp\* -Recurse -Force -ErrorAction SilentlyContinue -Exclude packer*.ps1
+            Write-EventLog -EventId 90 -Message "Removing all files not in use in $env:windir\TEMP" @EVT -EntryType Information @sHT
+            Write-Verbose -Message "Removing all files not in use in $env:windir\TEMP"
+            Remove-Item -Path $env:windir\Temp\* -Recurse -Force @sHT -Exclude packer*.ps1
 
             # Clear out Windows Error Reporting (WER) report archive folders
-            Write-EventLog -EventId 90 -Message "Cleaning up WER report archive" -LogName 'WDOT' -Source 'DiskCleanup' -EntryType Information
-            Write-Verbose "Cleaning up WER report archive"
-            Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
-            Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\ReportArchive\* -Recurse -Force -ErrorAction SilentlyContinue
-            Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\ReportQueue\* -Recurse -Force -ErrorAction SilentlyContinue
+            Write-EventLog -EventId 90 -Message "Cleaning up WER report archive" @EVT -EntryType Information @sHT
+            Write-Verbose -Message 'Cleaning up WER report archive'
+            Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\Temp\* -Recurse -Force @sHT
+            Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\ReportArchive\* -Recurse -Force @sHT
+            Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\ReportQueue\* -Recurse -Force @sHT
 
             # Delete not in-use anything in your %temp% folder
-            Write-EventLog -EventId 90 -Message "Removing files not in use in $env:temp directory" -LogName 'WDOT' -Source 'DiskCleanup' -EntryType Information
-            Write-Verbose "Removing files not in use in $env:temp directory"
-            Remove-Item -Path $env:TEMP\* -Recurse -Force -ErrorAction SilentlyContinue -Exclude packer*.ps1
+            Write-EventLog -EventId 90 -Message "Removing files not in use in $env:temp directory" @EVT -EntryType Information @sHT
+            Write-Verbose -Message "Removing files not in use in $env:temp directory"
+            Remove-Item -Path $env:TEMP\* -Recurse -Force @sHT -Exclude packer*.ps1
 
             # Clear out ALL visible Recycle Bins
-            Write-EventLog -EventId 90 -Message "Clearing out ALL Recycle Bins" -LogName 'WDOT' -Source 'DiskCleanup' -EntryType Information
-            Write-Verbose "Clearing out ALL Recycle Bins"
-            Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+            Write-EventLog -EventId 90 -Message 'Clearing out ALL Recycle Bins' @EVT -EntryType Information @sHT
+            Write-Verbose -Message 'Clearing out ALL Recycle Bins'
+            Clear-RecycleBin -Force @sHT
 
             # Clear out BranchCache cache
-            Write-EventLog -EventId 90 -Message "Clearing BranchCache cache" -LogName 'WDOT' -Source 'DiskCleanup' -EntryType Information
-            Write-Verbose "Clearing BranchCache cache"
-            Clear-BCCache -Force -ErrorAction SilentlyContinue
+            Write-EventLog -EventId 90 -Message 'Clearing BranchCache cache' @EVT -EntryType Information @sHT
+            Write-Verbose -Message 'Clearing BranchCache cache'
+            Clear-BCCache -Force @sHT
         }
         catch
         {
-            Write-Verbose "DiskCleanup encountered a non-critical error: $($_.Exception.Message)"
+            Write-Warning -Message "DiskCleanup encountered a non-critical error: $($_.Exception.Message)"
         }
 
     }
     End
     {
-        Write-Verbose "Exiting Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Exiting Function '$($MyInvocation.MyCommand.Name)'"
     }
 }
