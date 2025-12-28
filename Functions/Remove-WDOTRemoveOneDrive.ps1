8007027e7a13bea579572efa8ef2d7c50aff8558
Emin <emin.atac@gmail.com>
2025-12-28 14:17:09 +0100

    Remove-WDOTRemoveOneDrive.ps1 lisible improvements

--- Functions/Remove-WDOTRemoveOneDrive.ps1
+++ Functions/Remove-WDOTRemoveOneDrive.ps1
@@ -1,4 +1,5 @@
-﻿function Remove-WDOTRemoveOneDrive
+﻿#Requires -RunAsAdministrator
+Function Remove-WDOTRemoveOneDrive
 {
     [CmdletBinding(SupportsShouldProcess)]
 
@@ -9,21 +10,24 @@
 
     Begin
     {
-        Write-Verbose "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        $HT = @{ ErrorAction = 'Stop' }
+        $sHT = @{ ErrorAction = 'SilentlyContinue' }
+        EVT = @{ LogName = 'WDOT' ; Source = 'AdvancedOptimizations' }
     }
     Process
     {
-        Write-EventLog -EventId 80 -Message "Remove OneDrive Commercial" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
-        Write-Host "Windows Advanced Optimize] Removing OneDrive Commercial" -ForegroundColor Cyan
+        Write-EventLog -EventId 80 -Message "Remove OneDrive Commercial" @EVT -EntryType Information @sHT
+        Write-Host -Object "Windows Advanced Optimize] Removing OneDrive Commercial" -ForegroundColor Cyan
         $OneDrivePath = @('C:\Windows\System32\OneDriveSetup.exe', 'C:\Windows\SysWOW64\OneDriveSetup.exe')
         $OneDrivePath | ForEach-Object {
-            If (Test-Path $_)
+            If (Test-Path -Path $_ -PathType Leaf)
             {
-                Write-Host "`tAttempting to uninstall $_"
-                Write-EventLog -EventId 80 -Message "Commercial $_" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
+                Write-Host -Object "`tAttempting to uninstall $_"
+                Write-EventLog -EventId 80 -Message "Commercial $_" @EVT -EntryType Information @sHT
                 if ($PSCmdlet.ShouldProcess($_,'Start uninstall process')) {
                  try {
-                  Start-Process $_ -ArgumentList "/uninstall" -Wait -ErrorAction Stop
+                  Start-Process -FilePath $_ -ArgumentList '/uninstall' -Wait @HT
                  } catch {
                   Write-Warning -Message "Failed to start uninstall process because $($_.Exception.Message)"
                  }
@@ -31,13 +35,14 @@
             }
         }
 
-        Write-EventLog -EventId 80 -Message "Removing shortcut links for OneDrive" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
-        Get-ChildItem 'C:\*' -Recurse -Force -EA SilentlyContinue -Include 'OneDrive', 'OneDrive.exe', 'OneDrive.ico' | Where-Object { $_.FullName -notlike '*\WinSxS\*' } |
+        Write-EventLog -EventId 80 -Message "Removing shortcut links for OneDrive" @EVT -EntryType Information @sHT
+        Get-ChildItem -Path 'C:\*' -Recurse -Force @sHT -Include 'OneDrive', 'OneDrive.exe', 'OneDrive.ico' |
+        Where-Object { $_.FullName -notlike '*\WinSxS\*' } |
         Foreach-Object {
          $f = $_
          if ($PSCmdlet.ShouldProcess("$($f.FullName)",'Delete item')) {
           try {
-           $f | Remove-Item -Force -Recurse -ErrorAction Stop
+           $f | Remove-Item -Force -Recurse @HT
           } catch {
            Write-Warning -Message "Failed to delete $($f.FullName) because $($_.Exception.Message)"
           }
@@ -46,6 +51,6 @@
     }
     End
     {
-        Write-Verbose "Exiting Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Exiting Function '$($MyInvocation.MyCommand.Name)'"
     }
 }
