6120e704a98629b8b17caba264a657add8ce94bf
Emin <emin.atac@gmail.com>
2025-12-28 16:56:23 +0100

    Disable-WDOTServices.ps1 lisible improvements

--- Functions/Disable-WDOTServices.ps1
+++ Functions/Disable-WDOTServices.ps1
@@ -1,51 +1,60 @@
-﻿function Disable-WDOTService
-{
+﻿#Requires -RunAsAdministrator
+Function Disable-WDOTService {
     [CmdletBinding()]
-
     Param
     (
-
+     [Parameter()]
+     [string]$ServicesFilePath = ".\Services.json"
     )
-
     Begin
     {
-        Write-Verbose "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        $HT = @{ ErrorAction = 'Stop' }
+        $sHT = @{ ErrorAction = 'SilentlyContinue' }
+        EVT = @{ LogName = 'WDOT' ; Source = 'AdvancedOptimizations' }
     }
     Process
     {
-        $ServicesFilePath = ".\Services.json"
-        If (Test-Path $ServicesFilePath)
+        If (Test-Path -Path $ServicesFilePath -PathType Leaf)
         {
-            Write-EventLog -EventId 60 -Message "Disable Services" -LogName 'WDOT' -Source 'Services' -EntryType Information
-            Write-Host "[VDI Optimize] Disable Services" -ForegroundColor Cyan
-            $ServicesToDisable = (Get-Content $ServicesFilePath | ConvertFrom-Json ).Where( { $_.OptimizationState -eq 'Apply' })
+         try {
+            Write-EventLog -EventId 60 -Message "Disable Services" @EVT -EntryType Information
+            Write-Host -Object "[VDI Optimize] Disable Services" -ForegroundColor Cyan
+            $ServicesToDisable = (Get-Content -Path $ServicesFilePath @HT | ConvertFrom-Json @HT).Where( { $_.OptimizationState -eq 'Apply' })
 
             If ($ServicesToDisable.count -gt 0)
             {
-                Write-EventLog -EventId 60 -Message "Processing Services Configuration File" -LogName 'WDOT' -Source 'Services' -EntryType Information
-                Write-Verbose "Processing Services Configuration File"
+                Write-EventLog -EventId 60 -Message "Processing Services Configuration File" @EVT -EntryType Information @sHT
+                Write-Verbose -Message 'Processing Services Configuration File'
                 Foreach ($Item in $ServicesToDisable)
                 {
-                    Write-EventLog -EventId 60 -Message "Attempting to disable Service $($Item.Name) - $($Item.Description)" -LogName 'WDOT' -Source 'Services' -EntryType Information
-                    Write-Verbose "Attempting to disable Service $($Item.Name) - $($Item.Description)"
-                    Set-Service $Item.Name -StartupType Disabled
+                    Write-EventLog -EventId 60 -Message "Attempting to disable Service $($Item.Name) - $($Item.Description)" @EVT -EntryType Information @sHT
+                    Write-Verbose -Message "Attempting to disable Service $($Item.Name) - $($Item.Description)"
+                    try {
+                     Set-Service -Name $Item.Name -StartupType Disabled @HT
+                    } catch {
+                     Write-Warning -Message "Failed to change startup type of $($Item.Name) because $($_.Exception.Message)"
+                    }
                 }
             }
             Else
             {
-                Write-EventLog -EventId 60 -Message "No Services found to disable" -LogName 'WDOT' -Source 'Services' -EntryType Warning
-                Write-Verbose "No Services found to disable"
+                Write-EventLog -EventId 60 -Message "No Services found to disable" @EVT -EntryType Warning @sHT
+                Write-Verbose -Message 'No Services found to disable'
             }
+         } catch {
+          Write-Warning -Message "Failed to change services because $($_.Exception.Message)"
+         }
         }
         Else
         {
-            Write-EventLog -EventId 160 -Message "File not found: $ServicesFilePath" -LogName 'WDOT' -Source 'Services' -EntryType Error
-            Write-Warning "File not found: $ServicesFilePath"
+            Write-EventLog -EventId 160 -Message "File not found: $ServicesFilePath" @EVT -EntryType Error @sHT
+            Write-Warning -Message "File not found: $ServicesFilePath"
         }
 
     }
     End
     {
-        Write-Verbose "Exiting Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Exiting Function '$($MyInvocation.MyCommand.Name)'"
     }
 }
