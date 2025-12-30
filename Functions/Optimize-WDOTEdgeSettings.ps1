6907a3c32cf3d16b68f6275a9e69d1742703f59f
Emin <emin.atac@gmail.com>
2025-12-30 11:22:01 +0100

    Optimize-WDOTEdgeSettings.ps1 lisible improvements

--- Functions/Optimize-WDOTEdgeSettings.ps1
+++ Functions/Optimize-WDOTEdgeSettings.ps1
@@ -1,55 +1,79 @@
-﻿function Optimize-WDOTEdgeSetting
+﻿Function Optimize-WDOTEdgeSetting
 {
     [CmdletBinding()]
 
     Param
     (
-
+  [Parameter()]
+  [string]$EdgeFilePath = ".\EdgeSettings.json"
     )
 
     Begin
     {
-        Write-Verbose "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        $HT = @{ ErrorAction = 'Stop' }
+        $sHT = @{ ErrorAction = 'SilentlyContinue' }
+        $EVT = @{ LogName = 'WDOT' ; Source = 'AdvancedOptimizations' }
+        $eId80Info  = @{ EventId = 80 ; EntryType = 'Information' }
+        $eId80Warn  = @{ EventId = 80 ; EntryType = 'Warning' }
     }
     Process
     {
-        $EdgeFilePath = ".\EdgeSettings.json"
-        If (Test-Path $EdgeFilePath)
+
+        If (Test-Path -Path $EdgeFilePath -PathType Leaf)
         {
-            Write-EventLog -EventId 80 -Message "Edge Policy Settings" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
-            Write-Host "[Windows Advanced Optimize] Edge Policy Settings" -ForegroundColor Cyan
-            $EdgeSettings = Get-Content $EdgeFilePath | ConvertFrom-Json
+try {
+            Write-EventLog -Message "Edge Policy Settings" @EVT @sHT @eId80Info
+            Write-Host -Object "[Windows Advanced Optimize] Edge Policy Settings" -ForegroundColor Cyan
+            $EdgeSettings = Get-Content -Path $EdgeFilePath @HT | ConvertFrom-Json @HT
             If ($EdgeSettings.Count -gt 0)
             {
-                Write-EventLog -EventId 80 -Message "Processing Edge Policy Settings ($($EdgeSettings.Count) Hives)" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
-                Write-Verbose "Processing Edge Policy Settings ($($EdgeSettings.Count) Hives)"
+                Write-EventLog -Message "Processing Edge Policy Settings ($($EdgeSettings.Count) Hives)" @EVT @sHT @eId80Info
+                Write-Verbose -Message "Processing Edge Policy Settings ($($EdgeSettings.Count) Hives)"
                 Foreach ($Key in $EdgeSettings)
                 {
                     If ($Key.OptimizationState -eq 'Apply')
                     {
                         If ($key.RegItemValueName -eq 'DefaultAssociationsConfiguration')
                         {
-                            Copy-Item .\ConfigurationFiles\DefaultAssociationsConfiguration.xml $key.RegItemValue -Force
+                            try {
+                             Copy-Item -Path .\ConfigurationFiles\DefaultAssociationsConfiguration.xml $key.RegItemValue -Force @HT
+                            } catch {
+                             Write-Warning -Message "Failed to copy file because $($_.Exception.Message)"
+                            }
                         }
-                        If (Get-ItemProperty -Path $Key.RegItemPath -Name $Key.RegItemValueName -ErrorAction SilentlyContinue)
+                        If (Get-ItemProperty -Path $Key.RegItemPath -Name $Key.RegItemValueName @sHT)
                         {
-                            Write-EventLog -EventId 80 -Message "Found key, $($Key.RegItemPath) Name $($Key.RegItemValueName) Value $($Key.RegItemValue)" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
-                            Write-Verbose "Found key, $($Key.RegItemPath) Name $($Key.RegItemValueName) Value $($Key.RegItemValue)"
-                            Set-ItemProperty -Path $Key.RegItemPath -Name $Key.RegItemValueName -Value $Key.RegItemValue -Force
+                            Write-EventLog -Message "Found key, $($Key.RegItemPath) Name $($Key.RegItemValueName) Value $($Key.RegItemValue)" @EVT @sHT @eId80Info
+                            Write-Verbose -Message "Found key, $($Key.RegItemPath) Name $($Key.RegItemValueName) Value $($Key.RegItemValue)"
+                            try {
+                             Set-ItemProperty -Path $Key.RegItemPath -Name $Key.RegItemValueName -Value $Key.RegItemValue -Force @HT
+                            } catch {
+                             Write-Warning -Message "Failed to set $($Key.RegItemValueName) under $($Key.RegItemPath) because $($_.Exception.Message)"
+                            }
                         }
                         Else
                         {
-                            If (Test-path $Key.RegItemPath)
+                            If (Test-path -Path $Key.RegItemPath -PathType Container)
                             {
-                                Write-EventLog -EventId 80 -Message "Path found, creating new property -Path $($Key.RegItemPath) -Name $($Key.RegItemValueName) -PropertyType $($Key.RegItemValueType) -Value $($Key.RegItemValue)" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
-                                Write-Verbose "Path found, creating new property -Path $($Key.RegItemPath) Name $($Key.RegItemValueName) PropertyType $($Key.RegItemValueType) Value $($Key.RegItemValue)"
-                                New-ItemProperty -Path $Key.RegItemPath -Name $Key.RegItemValueName -PropertyType $Key.RegItemValueType -Value $Key.RegItemValue -Force | Out-Null
+                                Write-EventLog -Message "Path found, creating new property -Path $($Key.RegItemPath) -Name $($Key.RegItemValueName) -PropertyType $($Key.RegItemValueType) -Value $($Key.RegItemValue)" @EVT @sHT @eId80Info
+                                Write-Verbose -Message "Path found, creating new property -Path $($Key.RegItemPath) Name $($Key.RegItemValueName) PropertyType $($Key.RegItemValueType) Value $($Key.RegItemValue)"
+                                try {
+                                 $null = New-ItemProperty -Path $Key.RegItemPath -Name $Key.RegItemValueName -PropertyType $Key.RegItemValueType -Value $Key.RegItemValue -Force @HT
+                                } catch {
+                                 Write-Warning -Message "Faile to create $($Key.RegItemValueName) under $($Key.RegItemPath) because $($_.Exception.Message)"
+                                }
                             }
                             Else
                             {
-                                Write-EventLog -EventId 80 -Message "Creating Key and Path" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
-                                Write-Verbose "Creating Key and Path"
-                                New-Item -Path $Key.RegItemPath -Force | New-ItemProperty -Name $Key.RegItemValueName -PropertyType $Key.RegItemValueType -Value $Key.RegItemValue -Force | Out-Null
+                                Write-EventLog -Message "Creating Key and Path" @EVT @sHT @eId80Info
+                                Write-Verbose -Message 'Creating Key and Path'
+                                try {
+                                 $null = New-Item -Path $Key.RegItemPath -Force -ItemType Container @HT |
+                                 New-ItemProperty -Name $Key.RegItemValueName -PropertyType $Key.RegItemValueType -Value $Key.RegItemValue -Force @HT
+                                } catch {
+                                 Write-Warning -Message "Failed to add $($Key.RegItemValueName) under $($Key.RegItemPath) because $($_.Exception.Message)"
+                                }
                             }
 
                         }
@@ -58,18 +82,17 @@
             }
             Else
             {
-                Write-EventLog -EventId 80 -Message "No Edge Policy Settings Found!" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Warning
-                Write-Warning "No Edge Policy Settings found"
+                Write-EventLog -Message 'No Edge Policy Settings Found!' @EVT @sHT @eId80Warn
+                Write-Warning -Message 'No Edge Policy Settings found'
             }
-        }
-        Else
-        {
-            # nothing to do here"
+         } catch {
+          Write-Warning -Message "Failed to set Edge Settings because $($_.Exception.Message)"
+         }
         }
 
     }
     End
     {
-        Write-Verbose "Exiting Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Exiting Function '$($MyInvocation.MyCommand.Name)'"
     }
 }
