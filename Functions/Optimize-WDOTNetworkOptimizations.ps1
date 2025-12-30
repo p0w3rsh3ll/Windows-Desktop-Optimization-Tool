29566c63520e6610c2ff9806bb2d923d8261aaa0
Emin <emin.atac@gmail.com>
2025-12-30 11:22:57 +0100

    Optimize-WDOTNetworkOptimizations.ps1 lisible improvements

--- Functions/Optimize-WDOTNetworkOptimizations.ps1
+++ Functions/Optimize-WDOTNetworkOptimizations.ps1
@@ -1,84 +1,104 @@
-﻿function Optimize-WDOTNetworkOptimization
+﻿#Requires -RunAsAdministrator
+Function Optimize-WDOTNetworkOptimization
 {
     [CmdletBinding()]
-
     Param
     (
-
+    [Parameter()]
+    [string]$NetworkOptimizationsFilePath = ".\LanManWorkstation.json"
     )
-
     Begin
     {
-        Write-Verbose "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Entering Function '$($MyInvocation.MyCommand.Name)'"
+        $HT = @{ ErrorAction = 'Stop' }
+        $sHT = @{ ErrorAction = 'SilentlyContinue' }
+        $EVT = @{ LogName = 'WDOT' ; Source = 'DefaultUserSettings' }
+        $eId70Info  = @{ EventId = 40 ; EntryType = 'Information' }
+        $eId70Warn  = @{ EventId = 40 ; EntryType = 'Warning' }
     }
     Process
     {
-        $NetworkOptimizationsFilePath = ".\LanManWorkstation.json"
-        If (Test-Path $NetworkOptimizationsFilePath)
+        If (Test-Path -Path $NetworkOptimizationsFilePath -PathType Leaf)
         {
-            Write-EventLog -EventId 70 -Message "Configure LanManWorkstation Settings" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Information
-            Write-Host "[Windows Optimize] Configure LanManWorkstation Settings" -ForegroundColor Cyan
-            $LanManSettings = Get-Content $NetworkOptimizationsFilePath | ConvertFrom-Json
+         try {
+            Write-EventLog -Message "Configure LanManWorkstation Settings" @EVT @eId70Info @sHT
+            Write-Host -Object "[Windows Optimize] Configure LanManWorkstation Settings" -ForegroundColor Cyan
+            $LanManSettings = Get-Content $NetworkOptimizationsFilePath  @HT | ConvertFrom-Json @HT
             If ($LanManSettings.Count -gt 0)
             {
-                Write-EventLog -EventId 70 -Message "Processing LanManWorkstation Settings ($($LanManSettings.Count) Hives)" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Information
-                Write-Verbose "Processing LanManWorkstation Settings ($($LanManSettings.Count) Hives)"
+                Write-EventLog -Message "Processing LanManWorkstation Settings ($($LanManSettings.Count) Hives)" @EVT @eId70Info @sHT
+                Write-Verbose -Message "Processing LanManWorkstation Settings ($($LanManSettings.Count) Hives)"
                 Foreach ($Hive in $LanManSettings)
                 {
-                    If (Test-Path -Path $Hive.HivePath)
+                    If (Test-Path -Path $Hive.HivePath -PathType Container)
                     {
-                        Write-EventLog -EventId 70 -Message "Found $($Hive.HivePath)" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Information
-                        Write-Verbose "Found $($Hive.HivePath)"
-                        $Keys = $Hive.Keys.Where{ $_.OptimizationState -eq "Apply" }
+                        Write-EventLog -Message "Found $($Hive.HivePath)" @EVT @eId70Info @sHT
+                        Write-Verbose -Message "Found $($Hive.HivePath)"
+                        $Keys = $Hive.Keys.Where{ $_.OptimizationState -eq 'Apply' }
                         If ($Keys.Count -gt 0)
                         {
-                            Write-EventLog -EventId 70 -Message "Create / Update LanManWorkstation Keys" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Information
-                            Write-Verbose "Create / Update LanManWorkstation Keys"
+                            Write-EventLog -Message 'Create / Update LanManWorkstation Keys' @EVT @eId70Info @sHT
+                            Write-Verbose -Message 'Create / Update LanManWorkstation Keys'
                             Foreach ($Key in $Keys)
                             {
-                                If (Get-ItemProperty -Path $Hive.HivePath -Name $Key.Name -ErrorAction SilentlyContinue)
+                                If (Get-ItemProperty -Path $Hive.HivePath -Name $Key.Name @sHT)
                                 {
-                                    Write-EventLog -EventId 70 -Message "Setting $($Hive.HivePath) -Name $($Key.Name) -Value $($Key.PropertyValue)" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Information
-                                    Write-Verbose "Setting $($Hive.HivePath) -Name $($Key.Name) -Value $($Key.PropertyValue)"
-                                    Set-ItemProperty -Path $Hive.HivePath -Name $Key.Name -Value $Key.PropertyValue -Force
+                                    Write-EventLog -Message "Setting $($Hive.HivePath) -Name $($Key.Name) -Value $($Key.PropertyValue)" @EVT @eId70Info @sHT
+                                    Write-Verbose -Message "Setting $($Hive.HivePath) -Name $($Key.Name) -Value $($Key.PropertyValue)"
+                                    try {
+                                     Set-ItemProperty -Path $Hive.HivePath -Name $Key.Name -Value $Key.PropertyValue -Force @HT
+                                    } catch {
+                                     Write-Warning -Message "Failed to set $($Key.Name) under $($Hive.HivePath) because $($_.Exception.Message)"
+                                    }
                                 }
                                 Else
                                 {
-                                    Write-EventLog -EventId 70 -Message "New $($Hive.HivePath) -Name $($Key.Name) -Value $($Key.PropertyValue)" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Information
-                                    Write-Host "New $($Hive.HivePath) -Name $($Key.Name) -Value $($Key.PropertyValue)"
-                                    New-ItemProperty -Path $Hive.HivePath -Name $Key.Name -PropertyType $Key.PropertyType -Value $Key.PropertyValue -Force | Out-Null
+                                    Write-EventLog -Message "New $($Hive.HivePath) -Name $($Key.Name) -Value $($Key.PropertyValue)" @EVT @eId70Info @sHT
+                                    Write-Host -Object "New $($Hive.HivePath) -Name $($Key.Name) -Value $($Key.PropertyValue)"
+                                    try {
+                                     $null = New-ItemProperty -Path $Hive.HivePath -Name $Key.Name -PropertyType $Key.PropertyType -Value $Key.PropertyValue -Force @HT
+                                    } catch {
+                                     Write-Warning -Message "Failed to create $($Key.Name) under $($Hive.HivePath) because $($_.Exception.Message)"
+                                    }
                                 }
                             }
                         }
                         Else
                         {
-                            Write-EventLog -EventId 70 -Message "No LanManWorkstation Keys to create / update" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Warning
-                            Write-Warning "No LanManWorkstation Keys to create / update"
+                            Write-EventLog -Message 'No LanManWorkstation Keys to create / update' @EVT @eId70Warn @sHT
+                            Write-Warning -Message 'No LanManWorkstation Keys to create / update'
                         }
                     }
                     Else
                     {
-                        Write-EventLog -EventId 70 -Message "Registry Path not found $($Hive.HivePath)" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Warning
-                        Write-Warning "Registry Path not found $($Hive.HivePath)"
+                        Write-EventLog -Message "Registry Path not found $($Hive.HivePath)" @EVT @eId70Warn @sHT
+                        Write-Warning -Message "Registry Path not found $($Hive.HivePath)"
                     }
                 }
             }
             Else
             {
-                Write-EventLog -EventId 70 -Message "No LanManWorkstation Settings foun" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Warning
-                Write-Warning "No LanManWorkstation Settings found"
+                Write-EventLog -Message 'No LanManWorkstation Settings found' @EVT @eId70Warn @sHT
+                Write-Warning -Message 'No LanManWorkstation Settings found'
             }
+         } catch {
+          Write-Warning -Message "Failed to set LanManWorkstation Settings because $($_.Exception.Message)"
+         }
         }
         Else
         {
-            Write-EventLog -EventId 70 -Message "File not found - $NetworkOptimizationsFilePath" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Warning
-            Write-Warning "File not found - $NetworkOptimizationsFilePath"
+            Write-EventLog -Message "File not found - $($NetworkOptimizationsFilePath)" @EVT @eId70Warn @sHT
+            Write-Warning -Message "File not found - $($NetworkOptimizationsFilePath)"
         }
 
         # NIC Advanced Properties performance settings for network biased environments
-        Write-EventLog -EventId 70 -Message "Configuring Network Adapter Buffer Size" -LogName 'WDOT' -Source 'NetworkOptimizations' -EntryType Information
-        Write-Host "[Windows Optimize] Configuring Network Adapter Buffer Size" -ForegroundColor Cyan
-        Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB -NoRestart
+        Write-EventLog -Message "Configuring Network Adapter Buffer Size" @EVT @eId70Info @sHT
+        Write-Host -Object "[Windows Optimize] Configuring Network Adapter Buffer Size" -ForegroundColor Cyan
+        try {
+         Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB -NoRestart @HT
+        } catch {
+         Write-Waring -Message "Failed to set Send Buffer Size because $($_.Exception.Message)"
+        }
         <#  NOTE:
             Note that the above setting is for a Microsoft Hyper-V VM.  You can adjust these values in your environment...
             by querying in PowerShell using Get-NetAdapterAdvancedProperty, and then adjusting values using the...
@@ -88,6 +108,6 @@
     }
     End
     {
-        Write-Verbose "Exiting Function '$($MyInvocation.MyCommand.Name)'"
+        Write-Verbose -Message "Exiting Function '$($MyInvocation.MyCommand.Name)'"
     }
 }
